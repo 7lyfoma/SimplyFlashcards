864ba14254c740df03f257fac12d2b0d36fa6d55
7lyfoma <7lyfoma@gmail.com>
2024-06-03 11:15:18 +0100

    Set up testing with junit

---
+++ tests/SimpleTest.java
@@ -0,0 +1,13 @@
+package tests;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SimpleTest {
+    
+    @Test 
+    public void test() {
+        assertEquals(0,0);
+    }
+
+}
