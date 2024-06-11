c9647ec692211d16e0efb681c091ea481eab1d87
7lyfoma <7lyfoma@gmail.com>
2024-06-11 14:36:31 +0100

    start on filehandler

--- simplyflashcards/src/main/java/com/simplyflashcards/FileHandler.java
+++ simplyflashcards/src/main/java/com/simplyflashcards/FileHandler.java
@@ -1,5 +1,27 @@
 package com.simplyflashcards;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class FileHandler {
-    
+    public static FlashCardSet loadFlashCardSet(String filepath){
+        FlashCardSet fcs = new FlashCardSet();
+
+
+        File file = new File("filepath");
+        Scanner scanner;
+        try {
+            scanner = new Scanner(file);
+            while(scanner.hasNextLine()){
+                String data = scanner.nextLine();
+                //Todo here
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return fcs;
+       
+    }
 }
