599f5515b8e57eae9816f7f1f4e412130880a82b
7lyfoma <7lyfoma@gmail.com>
2024-06-11 14:11:59 +0100

    finish flashcardset

--- simplyflashcards/src/main/java/com/simplyflashcards/FlashCardSet.java
+++ simplyflashcards/src/main/java/com/simplyflashcards/FlashCardSet.java
@@ -7,7 +7,7 @@ public class FlashCardSet {
     HashMap<String, String> metaData;
 
     ArrayList<FlashCard> flashCardSet;
-    
+
     public FlashCardSet() {
         metaData = new HashMap<String, String>();
         flashCardSet = new ArrayList<FlashCard>();
@@ -21,8 +21,13 @@ public class FlashCardSet {
         return flashCardSet;
     }
 
+    public Boolean addFlashCard(FlashCard fc) {
+        return flashCardSet.add(fc);
+    }
 
-    
+    public Boolean removeFlashcard(FlashCard fc) {
+        return flashCardSet.remove(fc);
 
-    
+    }
+
 }
