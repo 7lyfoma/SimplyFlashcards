2ceca61a10897d97a89919b4247f05047342ea0d
7lyfoma <7lyfoma@gmail.com>
2024-06-11 11:01:29 +0100

    add assertj jars for swing testing, consider switching to maven

--- tests/SimpleTest.java
+++ tests/SimpleTest.java
@@ -1,6 +1,8 @@
 package tests;
 import static org.junit.Assert.assertEquals;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.Test;
 
 public class SimpleTest {
@@ -8,6 +10,7 @@ public class SimpleTest {
     @Test 
     public void test() {
         assertEquals(0,0);
+        assertThat(0).isEqualTo(0);
     }
 
 }
