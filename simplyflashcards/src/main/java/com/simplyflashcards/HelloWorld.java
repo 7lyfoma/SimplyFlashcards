f1e2f80c68567cd77e0f92479afe2338260eca4c
7lyfoma <7lyfoma@gmail.com>
2024-06-11 12:25:17 +0100

    convert project to maven

---
+++ simplyflashcards/src/main/java/com/simplyflashcards/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.simplyflashcards;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ simplyflashcards/src/main/java/com/simplyflashcards/Main.java
@@ -0,0 +1,7 @@
+package com.simplyflashcards;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
