6dd99608dbb30b365c6fd1fab2c033dd59c45b04
7lyfoma <7lyfoma@gmail.com>
2024-06-12 13:03:47 +0100

    update tests to avoid magic path directories hard coded

--- simplyflashcards/src/test/java/FileHandlerTests.java
+++ simplyflashcards/src/test/java/FileHandlerTests.java
@@ -1,5 +1,9 @@
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -11,15 +15,34 @@ public class FileHandlerTests {
     
 
     @Test 
-    public void testNameCorrect(){
-       String dir = System.getProperty("user.dir"); 
-       String newDir = dir + "\\src\\test\\testfiles\\cardset_size1.txt";      
-       FlashCardSet fcs = FileHandler.loadFlashCardSet(newDir);
+    public void testNameCorrect() throws IOException{
+        File f = File.createTempFile("temp", ".txt");
+        FileWriter fw = new FileWriter(f);
 
+        fw.write("name:cardset size 1\r\n" +
+                        ";\r\n" + 
+                        "front\r\n" + 
+                        "back\r\n" + 
+                        "fimg\r\n" + 
+                        "bimg\r\n" + 
+                        ";\r\n" + 
+                        "f\r\n" + 
+                        "b\r\n" + 
+                        "1\r\n" + 
+                        "2\r\n" + 
+                        ";");
 
+        fw.flush();
+        fw.close();
+
+       FlashCardSet fcs = FileHandler.loadFlashCardSet(f.getAbsolutePath());
+       
        assertEquals(fcs.getMetaData().get("name"), "cardset size 1");
 
        assertEquals("front", fcs.getFlashCardSet().get(0).getFrontText());
+
+
+       f.delete();
 
         
     }
